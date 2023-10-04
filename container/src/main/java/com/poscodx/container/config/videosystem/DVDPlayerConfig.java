fdd1b3e36cd15825a1777a53adde25f59a72bb6b
sso <sojeong7524@gmail.com>
2023-10-04 17:25:57 +0900

    container #4 (Explicit Java Config)

---
+++ container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
@@ -0,0 +1,7 @@
+package com.poscodx.container.config.videosystem;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DVDPlayerConfig {
+}
--- container/src/main/java/com/poscodx/container/soundsystem/HighSchoolRapper2.java
+++ container/src/main/java/com/poscodx/container/soundsystem/HighSchoolRapper2.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HighSchoolRapper2 implements CompactDisc {
+	private String title = "붕붕";
+	private String artist = "김하온";
+	
 	@Override
 	public String play() {
-		return "Playing 붕붕 by 김하온";
+		return "Playing " + title + " by " + artist;
 	}
 }
